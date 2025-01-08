7d6192f1c1b4b52006fd434a4f0b53a33851e563
19522003 <19522003@gm.uit.edu.vn>
2025-01-08 15:02:23 +0700

    add file path utilities to index.js for module path resolution

--- backend/src/index.js
+++ backend/src/index.js
@@ -8,6 +8,10 @@ import messageRoutes from "./routes/message.route.js";
 import cors from "cors";
 import { app, server } from "./socket.js";
 import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 config();
 
